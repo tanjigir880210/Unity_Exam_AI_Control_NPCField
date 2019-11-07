72c5d78c036348913214f91fac0d2ff0c78aff27
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2019-11-07 11:50:54 +0800

    期中

---
+++ UnityProject/Assets/scripts/Robot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [Header("對話")]
+    public string say = "hello! 機器球 可以幫我尋找5個電池 拜託 不然我會沒電       ";
+    public string saySpeed = "0.25";
+
+    [Header("任務")]
+    public bool complete;
+    public string bateryFinish = "5";
+    public string batery = "0";
+}
---
+++ UnityProject/Assets/scripts/Robotball.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Robotball : MonoBehaviour
+{
+    [Range(0, 1000), Header("跳躍")]
+    public float jump = 69;
+
+    [Range(0, 1000), Header("速度")]
+    public float speed = 50;
+
+    [Header("是否在地板上")]
+    public bool isGround = true;
+
+    private Rigidbody r3d;
+
+    void Start()
+    {
+
+        r3d = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        Walk();
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
+        r3d.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, speed * Input.GetAxis("Vertical")));
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r3d.AddForce(new Vector3(0, jump, 0));
+        }
+    }
+}
